03513fdff09c1976ee6bd91dbe95a55800c093a3
z0zero <briansangapta@gmail.com>
2024-03-06 14:26:43 +0700

    feat: Add landing pages with Rocket and Tailwind CSS

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#[macro_use] extern crate rocket;
+use rocket_dyn_templates::{Template};
+use std::collections::HashMap;
+
+#[get("/")]
+fn index() -> Template {
+    let context = HashMap::<String, String>::new();
+    Template::render("index", &context)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .attach(Template::fairing())
+        .mount("/", routes![index])
+}
